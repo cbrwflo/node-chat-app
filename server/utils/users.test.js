b226428b8c257fce2f117662d0e56c4a3a7381a0
cbrwflo <cbrwflo@users.noreply.github.com>
2017-07-31 15:49:21 -0700

    Add Users class and updateUserList event

---
+++ server/utils/users.test.js
@@ -0,0 +1,75 @@
+const expect = require('expect');
+
+const {Users} = require('./users');
+
+describe('Users', () => {
+  var users;
+
+  beforeEach(() => {
+    users = new Users();
+    users.users = [{
+      id: '0',
+      name: 'Mike',
+      room: 'Node Course'
+    }, {
+      id: '1',
+      name: 'Sanchez',
+      room: 'React Course'
+    }, {
+      id: '2',
+      name: 'Julie',
+      room: 'Node Course'
+    }];
+  });
+
+  it('should add new user', () => {
+    var users = new Users();
+
+    var user = {
+      id: '123',
+      name: 'Muchacho',
+      room: 'Nacho Libre fans'
+    };
+
+    var resUser = users.addUser(user.id, user.name, user.room);
+
+    expect(users.users).toEqual([user]);
+  });
+
+  it('should remove a user', () => {
+    var userId = '2';
+    var user = users.removeUser(userId);
+
+    users.removeUser(userId);
+    expect(user.id).toBe(userId);
+    expect(users.users.length).toEqual(2);
+  });
+
+  it('should not remove a user that does not exist', () => {
+    users.removeUser('doesNotExist');
+    expect(users.users.length).toEqual(3);
+  });
+
+  it('should find user', () => {
+    expect(users.getUser('0')).toEqual({
+      id: '0',
+      name: 'Mike',
+      room: 'Node Course'
+    });
+  });
+
+  it('should not find a user with an id that does not exist', () => {
+    expect(users.getUser('doesNotExist')).toNotExist();
+  });
+
+  it('should return names for node course', () => {
+    var userList = users.getUserList('Node Course');
+
+    expect(userList).toEqual(['Mike','Julie']);
+  });
+  it('should return names for react course', () => {
+    var userList = users.getUserList('React Course');
+
+    expect(userList).toEqual(['Sanchez']);
+  });
+});
